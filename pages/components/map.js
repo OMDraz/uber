8de16a4447af0e53349a6f187040d05eb37a9df3
Omar <omardraz94@gmail.com>
2021-11-28 11:25:55 -0800

    added a map and styling front page'

---
+++ pages/components/map.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import tw from "tailwind-styled-components"
+import mapboxgl from "mapbox-gl";
+import { useEffect } from 'react';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoib21hcmRyYXoiLCJhIjoiY2t3amxtc3lwMHFhNTJ1cXZtcjhkNmhpMCJ9.yw1LL0RskRr7J8NnzCQicw';
+
+
+const map = () => {
+
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+          center: [-99.29011, 39.172],
+          zoom: 4
+        });
+      });
+
+
+    return (
+        <Wrapper id='map'></Wrapper>
+    )
+}
+
+export default map
+
+const Wrapper = tw.div`
+    flex-1
+`
