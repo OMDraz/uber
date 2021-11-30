451e0785c987ac60f7a0b984addba2ee72017306
Omar <omardraz94@gmail.com>
2021-11-29 22:48:11 -0800

    small commit

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import tw from "tailwind-styled-components"
 import mapboxgl from "mapbox-gl";
 import { useEffect } from 'react';
 import Map from './components/map'
+import Link from 'next/link';
 
 
 export default function Home() {
@@ -16,12 +17,31 @@ export default function Home() {
           <Profile>
             <Name>Omar Draz</Name>
             <UserImage 
-              src='uber/pages/assets/image.jpeg'
+              src='https://i.picsum.photos/id/94/200/200.jpg?hmac=6A2OfXxXR04lSduBcURN4k0_3aiDGcJQjMpSwDmQevg'
             />
           </Profile>
         </Header>
+        <ActionButtons>
+        <Link href='/search'>
+            <ActionButton>
+                <ActionButtonImage src="https://i.ibb.co/cyvcpfF/uberx.png"/>
+                Ride
+            </ActionButton>
+          </Link>
+          <ActionButton>
+            <ActionButtonImage src="https://i.ibb.co/n776JLm/bike.png"/>
+            Wheels
+          </ActionButton>
+          <ActionButton>
+            <ActionButtonImage src="https://i.ibb.co/5RjchBg/uberschedule.png"/>
+            Reserve
+          </ActionButton>
+        </ActionButtons>
         {/* ActionButtons */}
         {/* InputButtons */}
+        <InputButton>
+          Where to?
+        </InputButton>
       </ActionItems>
     </Wrapper>
   )
@@ -33,11 +53,11 @@ const Wrapper = tw.div`
 `
 
 const ActionItems = tw.div`
-  flex-1
+  flex-1 p-4
 `
 
 const Header = tw.div`
-  
+  flex justify-between items-center
 `
 
 const UberLogo = tw.img`
@@ -45,13 +65,29 @@ const UberLogo = tw.img`
 `
 
 const Profile = tw.div`
-
+  flex items-center
 `
 
 const Name = tw.div`
-
+  mr-4 w-20 text-sm
 `
 
 const UserImage = tw.img`
+  h-12 w-12 rounded-full border border-gray-400 p-px
+`
 
+const ActionButtons = tw.div`
+  flex
+`
+
+const ActionButton = tw.div`
+  flex bg-gray-200 flex-1 m-1 h-32 items-center flex-col justify-center rounded-lg transform hover:scale-105 transition text-xl
+`
+
+const ActionButtonImage = tw.img`
+  h-3/5
+`
+
+const InputButton = tw.div`
+  h-20 bg-gray-200 text-2xl p-4 flex items-center mt-8
 `
---
+++ pages/search.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import tw from "tailwind-styled-components"
+
+const Search = () => {
+    return (
+        <Wrapper>
+            {/* Button Container */}
+            <ButtonContainer>
+                <BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png"/>
+            </ButtonContainer>
+            {/* Input container */}
+            {/* Saved Places */}
+            {/* Confirm Location */}
+        </Wrapper>
+    )
+}
+
+export default Search
+
+const Wrapper =  tw.div`
+
+`
+
+const ButtonContainer = tw.div`
+
+`
+
+const BackButton = tw.img`
+
+`
